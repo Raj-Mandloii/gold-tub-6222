9763dab48bbbf496fc3abf5f4c6abc30af754670
ranajit das <dasranajit410@gmail.com>
2022-08-26 05:29:37 +0530

    Day -3 added initially

--- hubspot/src/App.jsx
+++ hubspot/src/App.jsx
@@ -6,12 +6,14 @@ import FreeHubSpotCRM from "./Components/Software/FreeHubSpotCRM";
 import TabPage from "./Pages/TabsPage";
 import Pricing from "./Components/Price&PlansComponents/Pricing";
 import Login from "./Pages/Login";
+import OverviewOfAllProducts from "./Components/Software/OverviewOfAllProducts";
 function App() {
   return (
     <Box className="App">
       <Pricing />
       <Footer />
       <FreeHubSpotCRM />
+      <OverviewOfAllProducts/>
       <TabPage />
       <Login />
     </Box>
