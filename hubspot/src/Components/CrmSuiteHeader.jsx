51e4efa871fbf793492d743441805ec5b447dc5a
Sayan Mukherjee <sayan.mid590@gmail.com>
2022-08-28 10:41:41 +0530

    Revert "Fw18 1070 day 5"

--- hubspot/src/Components/CrmSuiteHeader.jsx
+++ hubspot/src/Components/CrmSuiteHeader.jsx
@@ -4,12 +4,10 @@ export default function CrmSuiteHeader(){
 
     return(
         <Box>
-            <a id="header">
             <Box>
                 <Text fontSize={"4xl"} fontWeight={"semibold"} color="rgb(51, 71, 91);" m={"5% 0 0 0"}>CRM Suite</Text>
                 <Text fontWeight={"light"} color="rgb(51, 71, 91);">Includes free tools, plus:</Text>
             </Box>
-            </a>
         </Box>
     )
 }
