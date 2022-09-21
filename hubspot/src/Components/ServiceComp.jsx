9f66078f09e9013a5e01576d8d856e750ed3c769
apoorvachikara <chikara02apoorva@gmail.com>
2022-09-21 21:24:32 +0530

    Use consistent style

--- hubspot/src/Components/ServiceComp.jsx
+++ hubspot/src/Components/ServiceComp.jsx
@@ -7,9 +7,11 @@ export default function ServiceComp({header,price}){
         <Box>
             <Box border={"1px solid cyan"} width="50%" ml={"6%"} mt="3%" mb={"3%"} p="1% 0 1% 0">
                 <Checkbox colorScheme='green'  textAlign={"left"} ml="3%">
-                    // Reviewed for: fw18_0133 and fw18_1070 - follow consistency in code - how your are displaying header
+                    {/* Reviewed for: fw18_0133 and fw18_1070 - follow consistency in code - how your are displaying header
+ */}
                     <Text fontWeight={"semibold"}>{header}/mo</Text>
-                   // Reviewed for: fw18_0133 and fw18_1070 - follow consistency in code - how your are displaying price
+                     {/* Reviewed for: fw18_0133 and fw18_1070 - follow consistency in code - how your are displaying price
+ */}
                     <Text fontWeight={"light"}>${price}</Text>
                     <Text fontWeight={"light"}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad facere ipsam sunt itaque autem veritatis dignissimos eaque harum est</Text>
                 </Checkbox>
