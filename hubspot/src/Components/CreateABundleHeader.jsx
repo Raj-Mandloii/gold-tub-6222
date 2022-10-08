23e3b3f9b3cb1401e58985492ee974738429fe5e
IntriguedSayan <sayan.mid590@gmail.com>
2022-10-08 14:00:19 +0530

    bundle responsive going on

--- hubspot/src/Components/CreateABundleHeader.jsx
+++ hubspot/src/Components/CreateABundleHeader.jsx
@@ -5,8 +5,8 @@ export default function CreateABundleHeader(){
     return(
         <Box>
             <Box>
-                <Text fontSize={"4xl"} fontWeight={"semibold"} color="rgb(51, 71, 91);" m={"5% 0 0 0"}>Choose products for your bundle.</Text>
-                <Text fontWeight={"light"} color="rgb(51, 71, 91);">Add the marketing, sales, and customer service software that your growing business needs.</Text>
+                <Text fontSize={{base:"xl",md:"2xl",lg:"4xl"}} fontWeight={"semibold"} color="rgb(51, 71, 91);" m={"5% 0 0 0"}>Choose products for your bundle.</Text>
+                <Text fontSize={{base:"smaller",md:"small",lg:"medium"}} fontWeight={"light"} color="rgb(51, 71, 91);">Add the marketing, sales, and customer service software that your growing business needs.</Text>
             </Box>
         </Box>
     )
